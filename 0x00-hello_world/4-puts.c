8f41de067cb2d75d05388cc091b72f1e9cf3fc81
aklil888 <aklilrose8@gmail.com>
2023-07-16 17:16:05 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual 
+ * main - prints "Programming is like building a multilingual
  * puzzle, followed by a new line.
  * Return: always 0.
  */
@@ -10,7 +10,8 @@
 
 int main(void)
 
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
