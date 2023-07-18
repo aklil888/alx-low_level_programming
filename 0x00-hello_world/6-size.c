39ccb97430686ce0bd09b560decc2594687695e3
aklil888 <aklilrose8@gmail.com>
2023-07-17 17:11:11 -0700

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-
-
 /**
  * main - prints the size of various types on the computer it is compiled and run on.
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
-
-
 int main(void)
-
-
 {
 	char a;
 	int b;
@@ -17,10 +11,10 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("size of a char: %lu bytr(s)\n", (unsigned long)sizeof(a));
-	printf("size of a int: %lu bytr(s)\n", (unsigned long)sizeof(b));
-	printf("size of a long int: %lu bytr(s)\n", (unsigned long)sizeof(c));
-	printf("size of a long long int: %lu bytr(s)\n", (unsigned long)sizeof(d));
-	printf("size of a float: %lu bytr(s)\n", (unsigned long)sizeof(f));
-	return (0)
+printf("size of a char: %lu bytr(s)\n", (unsigned long)sizeof(a));
+printf("size of a int: %lu bytr(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu bytr(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu bytr(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu bytr(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
